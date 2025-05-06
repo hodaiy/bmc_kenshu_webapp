e8da586848d5d4bb4546d609f3253105b9e83d38
hodaiy <hodait22@gmail.com>
2025-05-06 11:27:24 +0900

    Implemented third and fourth tasks

--- index.js
+++ index.js
@@ -12,85 +12,124 @@ const POSTS_FILE = path.join(__dirname, 'posts.txt');
 
 app.use(express.json());
 app.use(cookieParser());
-// 静的ファイル（CSSやJS）は誰でも見れるようにしておく
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/pages/login', express.static(path.join(__dirname,'public','login.html')));
 app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   '/pages/login',
+//   express.static(path.join(__dirname, 'public', 'login.html'))
+// );
+// app.use(
+//   '/pages/register',
+//   express.static(path.join(__dirname, 'public', 'register.html'))
+// );
+
+function md5(str) {
+  return crypto.createHash('md5').update(str).digest('hex');
+}
 
 function isValidToken(user, token) {
-    if (!fs.existsSync(TOKENS_FILE)) return (false);
-    const lines = fs.readFileSync(TOKENS_FILE, 'utf8').trim().split('\n');
-    return lines.some(line => {
-        const [u, t] = line.split(':');
-        return u === user && t === token;
-    });
+  if (!fs.existsSync(TOKENS_FILE)) return false;
+  const lines = fs.readFileSync(TOKENS_FILE, 'utf8').trim().split('\n');
+  return lines.some((line) => {
+    const [u, t] = line.split(':');
+    return u === user && t === token;
+  });
 }
 //トークン検証
 function authMiddleware(req, res, next) {
-    const { user, token } = req.cookies;
-    if (!user || !token || !isValidToken(user, token)) {
-        return res.redirect('/pages/login');
-    }
-    next();
+  const { user, token } = req.cookies;
+  if (!user || !token || !isValidToken(user, token)) {
+    return res.redirect('/pages/login');
+  }
+  next();
 }
 
 // index.html の表示はログイン必須にする
 app.get('/', authMiddleware, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/pages/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+app.get('/pages/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
 
+function loginMiddleware(req, res, next) {
+  const { user, password } = req.body;
+  if (!fs.existsSync(USERS_FILE))
+    return res.status(401).json({ error: 'No user' });
+  const lines = fs.readFileSync(USERS_FILE, 'utf8').trim().split('\n');
+  const isUser = lines.some((line) => {
+    const [u, p] = line.split(':');
+    return u === user && p === md5(password);
+  });
+  if (!isUser)
+    return res.status(401).json({
+      error: 'Username or Password is Invalid',
+    });
+  const token = crypto.randomBytes(16).toString('hex');
+  fs.appendFileSync(TOKENS_FILE, `${user}:${token}\n`);
+  res.cookie('user', user, { httpOnly: true });
+  res.cookie('token', token, { httpOnly: true });
+  res.redirect('/');
+  next();
+}
+app.post(
+  '/api/register',
+  (req, res, next) => {
+    const { user, password } = req.body;
+
+    if (!fs.existsSync(USERS_FILE)) fs.writeFileSync(USERS_FILE, '');
+    const lines = fs.readFileSync(USERS_FILE, 'utf8').trim().split('\n');
+    const isDefined = lines.some((line) => {
+      return user === line.split(':')[0];
+    });
+    if (isDefined)
+      return res.status(401).json({ error: 'User name already taken' });
+
+    const hash = md5(password);
+    fs.appendFileSync(USERS_FILE, `${user}:${hash}\n`);
+    next();
+  },
+  loginMiddleware
+);
 
 //パスワードが一致したらトークン生成
 //TODO:既にログインしている場合はホームリダイレクト
-app.post('/api/login', (req, res) => {
-    const { user, password } = req.body;
-    if (!fs.existsSync(USERS_FILE)) return res.status(401).json({ error: 'No user' });
-    const lines = fs.readFileSync(USERS_FILE, 'utf8').trim().split('\n');
-    const isUser = lines.some(line => {
-        const [u, p] = line.split(':');
-        return u === user && p === password;
-    });
-    if (!isUser) return res.status(401).json({
-        error: 'Username or Password is Invalid'
-    });
-    const token = crypto.randomBytes(16).toString('hex');
-    fs.appendFileSync(TOKENS_FILE, `${user}:${token}\n`);
-    res.cookie('user', user, { httpOnly: true });
-    res.cookie('token', token, { httpOnly: true });
-    res.redirect('/');
-})
+app.post('/api/login', loginMiddleware);
 
 //トークンが一致したらトークンファイルからその行を削除し、クライアントのクッキーもクリアする。
 //アカウント削除ではないのでユーザーファイルはいじらない。
-app.post('/api/logout', (req, res) => {
-    const { user, token } = req.cookies;
-    if (fs.existsSync(TOKENS_FILE)) {
-        const lines = fs.readFileSync(TOKENS_FILE, 'utf8').trim().split('\n');
-        const updated = lines.filter(line=>line!==`${user}:${token}`);
-        fs.writeFileSync(TOKENS_FILE, updated.join('\n') + '\n');
-    }
-    res.clearCookie('user');
-    res.clearCookie('token');
-    res.redirect('/pages/login');
+app.post('/api/logout', authMiddleware, (req, res) => {
+  const { user, token } = req.cookies;
+  if (fs.existsSync(TOKENS_FILE)) {
+    const lines = fs.readFileSync(TOKENS_FILE, 'utf8').trim().split('\n');
+    const updated = lines.filter((line) => line !== `${user}:${token}`);
+    fs.writeFileSync(TOKENS_FILE, updated.join('\n') + '\n');
+  }
+  res.clearCookie('user');
+  res.clearCookie('token');
+  res.redirect('/pages/login');
 });
 
-app.post('/api/posts',authMiddleware, (req, res) => {
-    const { user, date, start, end, comment } = req.body;
-    if (!/^[a-z]+$/.test(user)) {
-        return res.status(400).json({ error: 'Invalid username' });
-    }
-    const entry = JSON.stringify({ user, date, start, end, comment }) + '\n';
-    fs.appendFile(POSTS_FILE, entry, err => {
-        if (err) return res.status(500).json({ error: 'Failed to save posts' });
-       res.redirect('/');
-    });
+app.post('/api/posts', authMiddleware, (req, res) => {
+  const { user, date, start, end, comment } = req.body;
+  if (!/^[a-z]+$/.test(user)) {
+    return res.status(400).json({ error: 'Invalid username' });
+  }
+  const entry = JSON.stringify({ user, date, start, end, comment }) + '\n';
+  fs.appendFile(POSTS_FILE, entry, (err) => {
+    if (err) return res.status(500).json({ error: 'Failed to save posts' });
+    res.redirect('/');
+  });
 });
 
-app.get('/api/posts',authMiddleware, (req, res) => {
-    const lines = fs.readFileSync(POSTS_FILE, 'utf8').trim().split('\n');
-    const posts = lines.map(line =>JSON.parse(line));
-    res.json(posts);
- });
+app.get('/api/posts', authMiddleware, (req, res) => {
+  const lines = fs.readFileSync(POSTS_FILE, 'utf8').trim().split('\n');
+  const posts = lines.map((line) => JSON.parse(line));
+  res.json(posts);
+});
 
-
-app.listen(PORT, () => { console.log('Hello World') });
+app.listen(PORT, () => {
+  console.log('Hello World');
+});
